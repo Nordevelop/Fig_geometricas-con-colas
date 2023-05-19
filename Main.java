7bccc1c93a9d6e1eaac0bc7b742baaaa32fb780d
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-19 18:06:05 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package misClases;
+
+import java.util.*;
+import java.math.*;
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+import java.util.Qeue;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	   
+					Scanner entra=new Scanner(System.in);
+     }
+}
