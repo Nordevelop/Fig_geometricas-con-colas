cfef1098ab7835bac93511ddf270b0a79807daa4
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-19 17:44:56 -0300

    Update cuadrado.java

--- cuadrado.java
+++ cuadrado.java
@@ -1,17 +1,16 @@
 package misClases;
-
-import java.util.Stack;
-
+import java.util.LinkedList;
+import java.util.Queue;
 public class Cuadrado {
-	private Queue<Integer>Cola_CajaRectangular=null;
-	private int lado;
-	public Cuadrado(int d){
-  Cola_CajaRectangular = new LinkedList<Integer>();
-  this.lado4=d;
-	}
-	/*
-	 * metodos
-	 */
+   private Queue<Integer>Cola_CajaRectangular=null;
+   private int lado;
+   public Cuadrado(int d){
+   Cola_CajaRectangular = new LinkedList<Integer>();
+   this.lado4=d;
+  }
+  /*
+    * metodos
+    */
 	public int dar_Area_Cuadrado(){
 	return (this.lado*this.lado);
 	}
