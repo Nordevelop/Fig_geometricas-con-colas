9fa1c5cef7f71ff6665af79ae32d4d8b94e6e6e9
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-20 10:38:53 -0300

    Create Cubo.java

---
+++ Cubo.java
@@ -0,0 +1,13 @@
+package misClases;
+import java.math.*;
+
+public class Cubo {
+	/*propiedades de la clase cubo() 
+	 * encapsulados con acceso restringido 
+	 * fuera de la clase Cubo
+	 * author Nordevelop
+	 */
+	private double lado7;
+	private Stack<double>Pila_Cubo = null:
+	public Cubo(double u){ //constructor con argumento
+	   this.lado7=u;
