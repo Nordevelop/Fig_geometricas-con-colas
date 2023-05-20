85200ee72b8bad9fca3696b092311e536c098ffd
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-20 11:32:09 -0300

    Update Cubo.java

--- Cubo.java
+++ Cubo.java
@@ -7,7 +7,21 @@ public class Cubo {
 	 * fuera de la clase Cubo
 	 * author Nordevelop
 	 */
-	private double lado7;
+	private double lado4;
 	private Queue<Integer>Cola_Clindro=null;
-	public Cubo(double u){ //constructor con argumento
-	   this.lado7=u;
+	public Cubo(double g){ //constructor con argumento
+	Cola_Rectangulo = new LinkedList<Integer>();
+	   this.lado4=g;
+	}
+	/*
+	 * Metodos
+	 */
+	public double Dar_Area_Cubo(){
+		return (6*Math.pow(lado7, 2));
+	}
+	public double Dar_Volumen_Cubo(){
+		return (Math.pow(lado7, 3));
+	}
+}
+
+	
