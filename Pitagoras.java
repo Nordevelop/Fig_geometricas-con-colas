1f4c35a09dcfd59c5b6c174492a8f2f56363afc4
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-20 11:36:47 -0300

    Create Pitagoras.java

---
+++ Pitagoras.java
@@ -0,0 +1,20 @@
+package misClases;
+import java.util.Stack;
+import java.math.*;
+
+public class Pitagoras {
+  private Queue<Integer>Cola_Pitagoras=null;
+  private double cateto1;
+	private double cateto2;
+  public Pitagoras( double c1, double c2) {//Constructor con parametros
+		 Cola_Rectangulo = new LinkedList<double>();
+		this.cateto1=c1;
+		this.cateto2=c2;
+	}
+  public double dar_hipotenusa() {
+		return(double)Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+	}
+	public double dar_areaTriangulo_rectangulo() {
+		return(double)(cateto1 * cateto2)/2;
+	}
+}
