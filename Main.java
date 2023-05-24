38418972cf6181139159f58f83f85da257556c1b
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-24 10:04:24 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import java.math.*;
 import java.util.EmptyStackException;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 
 public class Main {
@@ -16,11 +17,13 @@ public class Main {
 	Cilindro l=null;
 	Circulo p=null;
 	Cubo s=null;
+	Esfera = t=null;
 	Queue<Cuadrado> Cola_Cuadrado = new Queue<Cuadrado>();
 	Queue<Rectangulo> Cola_Rectangulo = new Queue<Rectangulo>; 
 	Queue<Cilindro> Cola_Cilindro = new Queue<Cilindro>; 
 	Queue<Circulo> Cola_Circulo = new Queue<Circulo>; 
 	Queue<Cubo> Cola_Cubo = new Queue<Cubo>; 
+	Queue<Esfera> Cola_Esfera = new Queue<Esfera>; 
 	
 	
      }
